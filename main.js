b375af756f7184f767f28e6894cb92cf2c55fcd2
Vico <vpontin93@gmail.com>
2019-04-11 09:06:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* MÓDULOS */
 const fs = require('fs');
+const path = require('path');
 
 /* VARIÁVEIS */
 
@@ -14,11 +15,25 @@ const MEME_FOLDER = './memes'; // Output folder
 /* FUNÇÕES PRINCIPAIS */
 
 // Sorteia um template aleatório
-function get_template()
+/* function get_template()
 {
 	// Lê a lista de arquivos dentro da pasta de sources
-	var file_list = fs.readdirSync(TEMPLATE_FOLDER + '/*.json');
+	var file_list = fs.readdirSync(TEMPLATE_FOLDER);
 	// Exibe e retorna o nome de um arquivo aleatório da pasta
+	var file_choosed = file_list[Math.floor(Math.random() * file_list.length)];
+	while (path.extname(file_choosed).toLowerCase() !== '.json')
+	{
+		file_choosed = file_list[Math.floor(Math.random() * file_list.length)];
+	}
+	console.log('[INFO] Template sorteada: ' + file_choosed);
+	return file_choosed;
+} */
+
+function get_template()
+{
+	// Lê a lista de arquivos dentro da pasta de sources (já filtrando os arquivos .json
+	var file_list = fs.readdirSync(TEMPLATE_FOLDER).filter(function(f){return path.extname(f).toLowerCase() === '.json';});
+	// Exibe e retorna o nome de um JSON aleatório da pasta
 	var file_choosed = file_list[Math.floor(Math.random() * file_list.length)];
 	console.log('[INFO] Template sorteada: ' + file_choosed);
 	return file_choosed;
@@ -38,8 +53,10 @@ function get_source()
 // Cria o meme
 function make_meme()
 {
-	get_source(); // DEBUG
-	get_template(); // DEBUG
+	var template_json = get_template();
+	var template_image = template_json.replace("json", "png");
+	// get image
+	// insert json data into image
 }
 
 // Função principal
