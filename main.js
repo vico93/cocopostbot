0b3a961163e06c5499017bd46d60feb121a92b5a
Vico <vpontin93@gmail.com>
2019-04-11 07:34:03 -0300

    O que fiz até agora

--- main.js
+++ main.js
@@ -16,24 +16,38 @@ const MEME_FOLDER = './memes'; // Output folder
 // Sorteia um template aleatório
 function get_template()
 {
-	
+	// Lê a lista de arquivos dentro da pasta de sources
+	var file_list = fs.readdirSync(TEMPLATE_FOLDER + '/*.json');
+	// Exibe e retorna o nome de um arquivo aleatório da pasta
+	var file_choosed = file_list[Math.floor(Math.random() * file_list.length)];
+	console.log('[INFO] Template sorteada: ' + file_choosed);
+	return file_choosed;
 }
 
 // Sorteia uma source aleatória
 function get_source()
 {
-	var file_list = fs.readdirSync(SOURCE_FOLDER); // Lê a lista de arquivos dentro da pasta de sources
-	return file_list[Math.floor(Math.random() * file_list.length)]; // Retorna o nome de um arquivo aleatório da pasta
+	// Lê a lista de arquivos dentro da pasta de sources
+	var file_list = fs.readdirSync(SOURCE_FOLDER);
+	// Exibe e retorna o nome de um arquivo aleatório da pasta
+	var file_choosed = file_list[Math.floor(Math.random() * file_list.length)];
+	console.log('[INFO] Source sorteada: ' + file_choosed);
+	return file_choosed;
 }
 
 // Cria o meme
 function make_meme()
 {
-	
+	get_source(); // DEBUG
+	get_template(); // DEBUG
 }
 
 // Função principal
 function main()
 {
-	make_meme();
+	console.log('[INFO] Bot iniciado!');
+	setInterval(make_meme, 1500); // DEBUG
+	// make_meme(); // DEBUG
 }
+
+main();
